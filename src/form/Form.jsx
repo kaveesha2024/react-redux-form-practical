e983e644b60dc02a39c020ff63a1df0b68418ff1
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-03-08 20:31:24 +0530

    Added a heading and change name of the btn

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -4,14 +4,19 @@ const Form = () => {
   return (
     <div>
       <form className="max-w-sm mx-auto mt-10">
+        <div>
+          <h1 className="font-bold text-lg mb-5 flex justify-center">
+            Product Basic Details
+          </h1>
+        </div>
         <InputField id="name" type="text" label="Enter Product Name" />
         <InputField id="quantity" type="number" label="Product Quantity" />
         <InputField id="price" type="number" label="Product Price" />
         <button
-          type="submit"
+          type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         >
-          Register new account
+          Next
         </button>
       </form>
     </div>
